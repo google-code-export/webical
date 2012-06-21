fae57d35706f5c1fa4f25e01cbbd1683fd8d91ed
hz0885@gmail.com <hz0885@gmail.com@e78ab395-e342-0410-bda9-91e046cd5ffa>
2012-06-21 13:57:49 +0000

    94: Take into account events with start-/end-date outside of period and sort in dayview

--- trunk/webical-core/src/main/java/org/webical/web/component/calendar/model/EventsModel.java
+++ trunk/webical-core/src/main/java/org/webical/web/component/calendar/model/EventsModel.java
@@ -4,7 +4,7 @@
  *
  *    This file is part of Webical.
  *
- *    $Id: $
+ *    $Id$
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
