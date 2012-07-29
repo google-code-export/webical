1a31d78ad8c753f7b12a5b24cc5edf6c7cd6204b
hz0885@gmail.com <hz0885@gmail.com@e78ab395-e342-0410-bda9-91e046cd5ffa>
2012-07-29 19:51:51 +0000

    0: Add toString method

--- trunk/webical-core/src/main/java/org/webical/UserSettings.java
+++ trunk/webical-core/src/main/java/org/webical/UserSettings.java
@@ -1,3 +1,25 @@
+/*
+ *    Webical - http://code.google.com/p/webical/
+ *    Copyright (C) 2012 by Cebuned
+ *
+ *    This file is part of Webical.
+ *
+ *    $Id$
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.webical;
 
 import org.webical.manager.WebicalException;
@@ -9,8 +31,8 @@ public class UserSettings extends Settings {
 	private static final int DEFAULT_FIRST_DAY_OF_WEEK = 1;
 	private static final int DEFAULT_NUMBER_OF_AGENDA_DAYS = 4;
 
-	private User user;
-	private int defaultCalendarView, firstDayOfWeek, numberOfAgendaDays;
+	private User user = null;
+	private Integer defaultCalendarView, firstDayOfWeek, numberOfAgendaDays;
 	private String dateFormat, timeFormat;
 
 	public UserSettings() { }
@@ -28,7 +50,6 @@ public class UserSettings extends Settings {
 	public User getUser() {
 		return user;
 	}
-
 	/**
 	 * @param user the user to set
 	 */
@@ -36,34 +57,30 @@ public class UserSettings extends Settings {
 		this.user = user;
 	}
 
-	public int getDefaultCalendarView() {
+	public Integer getDefaultCalendarView() {
 		return defaultCalendarView;
 	}
-
-	public void setDefaultCalendarView(int defaultCalendarView) {
+	public void setDefaultCalendarView(Integer defaultCalendarView) {
 		this.defaultCalendarView = defaultCalendarView;
 	}
 
-	public int getFirstDayOfWeek() {
+	public Integer getFirstDayOfWeek() {
 		return firstDayOfWeek;
 	}
-
-	public void setFirstDayOfWeek(int firstDayOfWeek) {
+	public void setFirstDayOfWeek(Integer firstDayOfWeek) {
 		this.firstDayOfWeek = firstDayOfWeek;
 	}
 
-	public int getNumberOfAgendaDays() {
+	public Integer getNumberOfAgendaDays() {
 		return numberOfAgendaDays;
 	}
-
-	public void setNumberOfAgendaDays(int numberOfAgendaDays) {
+	public void setNumberOfAgendaDays(Integer numberOfAgendaDays) {
 		this.numberOfAgendaDays = numberOfAgendaDays;
 	}
 
 	public String getDateFormat() {
 		return dateFormat;
 	}
-
 	public void setDateFormat(String dateFormat) {
 		this.dateFormat = dateFormat;
 	}
@@ -71,11 +88,10 @@ public class UserSettings extends Settings {
 	public String getTimeFormat() {
 		return timeFormat;
 	}
-
 	public void setTimeFormat(String timeFormat) {
 		this.timeFormat = timeFormat;
 	}
-	
+
 	// TODO mattijs: let default user settings depend on locale
 	public void createDefaultSettings() {
 		this.defaultCalendarView = DEFAULT_DEFAULT_CALENDAR_VIEW;
@@ -85,4 +101,15 @@ public class UserSettings extends Settings {
 		this.timeFormat = "HH:mm";
 	}
 
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder(this.getClass().getSimpleName());
+		sb.append(" User:" + getUser().getUserId());
+		sb.append(" defaultCalendarView:" + getDefaultCalendarView());
+		sb.append(" firstDayOfWeek:" + getFirstDayOfWeek());
+		sb.append(" dateFormat:" + getDateFormat());
+		sb.append(" timeFormat:" + getTimeFormat());
+		return sb.toString();
+	}
 }
