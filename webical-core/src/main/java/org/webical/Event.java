6c82493af175b43248e918fc0dfd9fbe14ac96df
hz0885@gmail.com <hz0885@gmail.com@e78ab395-e342-0410-bda9-91e046cd5ffa>
2012-08-04 14:50:15 +0000

    0: Include AllDay property with event copy

--- webical-core/src/main/java/org/webical/Event.java
+++ webical-core/src/main/java/org/webical/Event.java
@@ -4,6 +4,8 @@
  *
  *    This file is part of Webical.
  *
+ *    $Id$
+ *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
@@ -42,91 +44,85 @@ public class Event implements Serializable {
 	//TODO alarmc
 
 	//Foreign Key
-	private Calendar calendar;
+	private Calendar calendar = null;
 
 	//Database id
-	private Long eventId;
+	private Long eventId = null;
 
 	//Optional
-	private String clazz;
-	private Date created;
-	private String description;
-	private Date dtStart;
-	private String geo;
-	private Date lastMod;
-	private String location;
-	private String organizer;
-	private Integer priority;
-	private Date dtStamp;
-	private Integer seq;
-	private String status;
-	private String summary;
-	private String transp;
-	private String uid;
-	private String url;
-	private boolean allDay;
+	private String clazz = null;
+	private Date created = null;
+	private String description = null;
+	private Date dtStart = null;
+	private String geo = null;
+	private Date lastMod = null;
+	private String location = null;
+	private String organizer = null;
+	private Integer priority = null;
+	private Date dtStamp = null;
+	private Integer seq = null;
+	private String status = null;
+	private String summary = null;
+	private String transp = null;
+	private String uid = null;
+	private String url = null;
+	private Boolean allDay = Boolean.FALSE;
 
 	//Must not occur in the same eventprop
-	private Date dtEnd;
-	private String duration;
+	private Date dtEnd = null;
+	private String duration = null;
 
 	//May occur more than once
-	private Set<String> attach;
-	private Set<String> attendee;
-	private Set<String> categories;
-	private Set<String> comment;
-	private Set<String> contact;
-	private Set<String> rStatus;
-	private Set<String> related;
-	private Set<String> resources;
-	private Map<String, String> xProps;
+	private Set<String> attach = null;
+	private Set<String> attendee = null;
+	private Set<String> categories = null;
+	private Set<String> comment = null;
+	private Set<String> contact = null;
+	private Set<String> rStatus = null;
+	private Set<String> related = null;
+	private Set<String> resources = null;
+	private Map<String, String> xProps = null;
 
 	//Recurrence
-	private Set<Date> rDate;
-	private Set<String> rRule;
-	private Set<Date> exDate;
-	private Set<String> exRule;
+	private Set<Date> rDate = null;
+	private Set<String> rRule = null;
+	private Set<Date> exDate = null;
+	private Set<String> exRule = null;
 
-	/** Getters & Setters */	
-	public boolean isAllDay(){
+	/** Getters & Setters */
+	public Boolean isAllDay() {
 		return allDay;
 	}
-	public void setAllDay(boolean allday) {
+	public void setAllDay(Boolean allday) {
 		this.allDay = allday;
 	}
-	
+
 	public Set<String> getAttach() {
-		if(attach == null){
+		if (attach == null) {
 			attach = new HashSet<String>();
 		}
-
 		return attach;
 	}
-
 	public void setAttach(Set<String> attach) {
 		this.attach = attach;
 	}
 
 	public Set<String> getAttendee() {
-		if(attendee == null){
+		if (attendee == null) {
 			attendee = new HashSet<String>();
 		}
-
 		return attendee;
 	}
-
 	public void setAttendee(Set<String> attendee) {
 		this.attendee = attendee;
 	}
 
 	public Set<String> getCategories() {
-		if(categories == null){
+		if (categories == null) {
 			categories = new HashSet<String>();
 		}
-
 		return categories;
 	}
-
 	public void setCategories(Set<String> categories) {
 		this.categories = categories;
 	}
@@ -134,31 +130,26 @@ public class Event implements Serializable {
 	public String getClazz() {
 		return clazz;
 	}
-
 	public void setClazz(String clazz) {
 		this.clazz = clazz;
 	}
 
 	public Set<String> getComment() {
-		if(comment == null){
+		if (comment == null) {
 			comment = new HashSet<String>();
 		}
-
 		return comment;
 	}
-
 	public void setComment(Set<String> comment) {
 		this.comment = comment;
 	}
 
 	public Set<String> getContact() {
-		if(contact == null){
+		if (contact == null) {
 			contact = new HashSet<String>();
 		}
-
 		return contact;
 	}
-
 	public void setContact(Set<String> contact) {
 		this.contact = contact;
 	}
@@ -166,7 +157,6 @@ public class Event implements Serializable {
 	public Date getCreated() {
 		return created;
 	}
-
 	public void setCreated(Date created) {
 		this.created = created;
 	}
@@ -174,7 +164,6 @@ public class Event implements Serializable {
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
@@ -182,7 +171,6 @@ public class Event implements Serializable {
 	public Date getDtEnd() {
 		return dtEnd;
 	}
-
 	public void setDtEnd(Date dtEnd) {
 		this.dtEnd = dtEnd;
 	}
@@ -190,7 +178,6 @@ public class Event implements Serializable {
 	public Date getDtStamp() {
 		return dtStamp;
 	}
-
 	public void setDtStamp(Date dtStamp) {
 		this.dtStamp = dtStamp;
 	}
@@ -198,7 +185,6 @@ public class Event implements Serializable {
 	public Date getDtStart() {
 		return dtStart;
 	}
-
 	public void setDtStart(Date dtStart) {
 		this.dtStart = dtStart;
 	}
@@ -206,31 +192,26 @@ public class Event implements Serializable {
 	public String getDuration() {
 		return duration;
 	}
-
 	public void setDuration(String duration) {
 		this.duration = duration;
 	}
 
 	public Set<Date> getExDate() {
-		if(exDate == null){
+		if (exDate == null) {
 			exDate = new HashSet<Date>();
 		}
-
 		return exDate;
 	}
-
 	public void setExDate(Set<Date> exDate) {
 		this.exDate = exDate;
 	}
 
 	public Set<String> getExRule() {
-		if(exRule == null){
+		if (exRule == null) {
 			exRule = new HashSet<String>();
 		}
-
 		return exRule;
 	}
-
 	public void setExRule(Set<String> exRule) {
 		this.exRule = exRule;
 	}
@@ -238,7 +219,6 @@ public class Event implements Serializable {
 	public String getGeo() {
 		return geo;
 	}
-
 	public void setGeo(String geo) {
 		this.geo = geo;
 	}
@@ -246,7 +226,6 @@ public class Event implements Serializable {
 	public Date getLastMod() {
 		return lastMod;
 	}
-
 	public void setLastMod(Date lastMod) {
 		this.lastMod = lastMod;
 	}
@@ -254,7 +233,6 @@ public class Event implements Serializable {
 	public String getLocation() {
 		return location;
 	}
-
 	public void setLocation(String location) {
 		this.location = location;
 	}
@@ -262,7 +240,6 @@ public class Event implements Serializable {
 	public String getOrganizer() {
 		return organizer;
 	}
-
 	public void setOrganizer(String organizer) {
 		this.organizer = organizer;
 	}
@@ -270,25 +247,22 @@ public class Event implements Serializable {
 	public Integer getPriority() {
 		return priority;
 	}
-
 	public void setPriority(Integer priority) {
 		this.priority = priority;
 	}
 
 	public Set<Date> getrDate() {
-		if(rDate == null){
+		if (rDate == null) {
 			rDate = new HashSet<Date>();
 		}
-
 		return rDate;
 	}
 
 	public void setrDate(Set<Date> date) {
 		rDate = date;
 	}
-
 	public Set<String> getRelated() {
-		if(related == null){
+		if (related == null) {
 			related = new HashSet<String>();
 		}
 		return related;
@@ -297,38 +271,32 @@ public class Event implements Serializable {
 	public void setRelated(Set<String> related) {
 		this.related = related;
 	}
-
 	public Set<String> getResources() {
-		if(resources == null){
+		if (resources == null) {
 			resources = new HashSet<String>();
 		}
-
 		return resources;
 	}
 
 	public void setResources(Set<String> resources) {
 		this.resources = resources;
 	}
-
 	public Set<String> getrRule() {
-		if(rRule == null){
+		if (rRule == null) {
 			rRule = new HashSet<String>();
 		}
-
 		return rRule;
 	}
 
 	public void setrRule(Set<String> rule) {
 		rRule = rule;
 	}
-
 	public Set<String> getrStatus() {
-		if(rStatus == null){
+		if (rStatus == null) {
 			rStatus = new HashSet<String>();
 		}
 		return rStatus;
 	}
-
 	public void setrStatus(Set<String> status) {
 		rStatus = status;
 	}
@@ -336,7 +304,6 @@ public class Event implements Serializable {
 	public Integer getSeq() {
 		return seq;
 	}
-
 	public void setSeq(Integer seq) {
 		this.seq = seq;
 	}
@@ -344,7 +311,6 @@ public class Event implements Serializable {
 	public String getStatus() {
 		return status;
 	}
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
@@ -352,7 +318,6 @@ public class Event implements Serializable {
 	public String getSummary() {
 		return summary;
 	}
-
 	public void setSummary(String summary) {
 		this.summary = summary;
 	}
@@ -360,7 +325,6 @@ public class Event implements Serializable {
 	public String getTransp() {
 		return transp;
 	}
-
 	public void setTransp(String transp) {
 		this.transp = transp;
 	}
@@ -368,7 +332,6 @@ public class Event implements Serializable {
 	public String getUid() {
 		return uid;
 	}
-
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
@@ -376,7 +339,6 @@ public class Event implements Serializable {
 	public String getUrl() {
 		return url;
 	}
-
 	public void setUrl(String url) {
 		this.url = url;
 	}
@@ -384,7 +346,6 @@ public class Event implements Serializable {
 	public Map<String, String> getxProps() {
 		return xProps;
 	}
-
 	public void setxProps(Map<String, String> props) {
 		xProps = props;
 	}
@@ -392,18 +353,16 @@ public class Event implements Serializable {
 	public Calendar getCalendar() {
 		return calendar;
 	}
-
 	public void setCalendar(Calendar calendar) {
 		this.calendar = calendar;
 	}
 
 	public String getXProp(String key) {
-		if(xProps == null || key ==null) {
+		if (xProps == null || key ==null) {
 			return null;
 		}
 		return xProps.get(key);
 	}
-
 	public void addXProp(String key, String value) {
 		if(xProps == null) {
 			xProps = new HashMap<String, String>();
@@ -414,7 +373,6 @@ public class Event implements Serializable {
 	public Long getEventId() {
 		return eventId;
 	}
-
 	public void setEventId(Long eventId) {
 		this.eventId = eventId;
 	}
@@ -446,7 +404,23 @@ public class Event implements Serializable {
 
 	@Override
 	public String toString() {
-		return "UID: " + uid + ", SUMMARY: " + summary;
+		StringBuffer sbts = new StringBuffer();
+		if (getUid() != null)
+		{
+			sbts.append("UID: ");
+			sbts.append(getUid());
+		}
+		if (getSummary() != null)
+		{
+			sbts.append(", SUMMARY: ");
+			sbts.append(getSummary());
+		}
+		if (getCalendar() != null)
+		{
+			sbts.append(", Calendar: ");
+			sbts.append(getCalendar().getName());
+		}
+		return sbts.toString();
 	}
 
 	public Event copyEvent() {
@@ -454,8 +428,9 @@ public class Event implements Serializable {
 		event.setSummary(this.getSummary());
 		event.setDtStart(this.getDtStart());
 		event.setDtEnd(this.getDtEnd());
-		event.setDescription(this.getDescription());
+		event.setAllDay(this.isAllDay());
 		event.setLocation(this.getLocation());
+		event.setDescription(this.getDescription());
 
 		event.setrRule(this.getrRule());
 		event.setExDate(this.getExDate());
@@ -463,5 +438,4 @@ public class Event implements Serializable {
 
 		return event;
 	}
-
 }
